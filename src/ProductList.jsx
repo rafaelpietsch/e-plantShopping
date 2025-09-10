cd06a254e1ed8e183d745ceb7c5cd02335f5d6e5
Rafael Pietsch <rafaelpietsch@outlook.com>
2025-09-09 21:52:15 -0400

    Corrected cart number and add to cart button functionalities

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -11,6 +11,9 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({}); // Products Added to Cart
     
+    const cartItems = useSelector((state) => state.cart.items);
+    const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -262,17 +265,11 @@ function ProductList({ onHomeClick }) {
 
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
-        const calculateTotalQuantity = () => {
-            let CartItems = 0;
-            const itemCost = parseFloat(item.cost.substring(1));
-            return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
-        };
-
+        
         setAddedToCart((prevState) => ({
             ...prevState,
             [product.name]: true,
         }));
-        calculateTotalQuantity();
     };
 
     
@@ -293,7 +290,19 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> 
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <h1 className='cart'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                <rect width="156" height="156" fill="none"></rect>
+                                <circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                            </svg>
+                            <span className="cart_quantity_count">{totalQuantity}</span>
+                        </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -316,10 +325,11 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-description">{plant.description}</div> {/* Display plant description */}
                                 <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                                 <button
-                                    className="product-button"
-                                    onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
+                                    className={addedToCart[plant.name] ? "product-button.added-to-cart" : "product-button"}
+                                    onClick={() => handleAddToCart(plant)} // The handler should now set addedToCart to true
+                                    disabled={addedToCart[plant.name]}
                                 >
-                                    Add to Cart
+                                    {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
                                 </button>
                                 </div>
                             ))}
